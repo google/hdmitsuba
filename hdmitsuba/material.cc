0d6ff834104e555eb7a76ee8ecc7b174341a8c7f
Delio Vicini <vicini@google.com>
2026-07-01 11:03:50 +0200

    Transition material.cc to use scene index APIs

--- hdmitsuba/material.cc
+++ hdmitsuba/material.cc
@@ -19,9 +19,14 @@
 #include <pxr/base/vt/value.h>
 #include <pxr/imaging/hd/changeTracker.h>
 #include <pxr/imaging/hd/material.h>
-#include <pxr/imaging/hd/materialNetwork2Interface.h>
+#include <pxr/imaging/hd/materialConnectionSchema.h>
+#include <pxr/imaging/hd/materialNetworkSchema.h>
+#include <pxr/imaging/hd/materialNodeParameterSchema.h>
+#include <pxr/imaging/hd/materialNodeSchema.h>
+#include <pxr/imaging/hd/materialSchema.h>
 #include <pxr/imaging/hd/renderDelegate.h>
 #include <pxr/imaging/hd/sceneDelegate.h>
+#include <pxr/imaging/hd/sceneIndex.h>
 #include <pxr/imaging/hd/types.h>
 #include <pxr/pxr.h>
 
@@ -32,6 +37,87 @@
 
 PXR_NAMESPACE_OPEN_SCOPE
 
+namespace {
+
+// Temporarily convert from material network schema to material network2.
+// In a later step, we will modify the prim_translator to directly work
+// using the schema.
+HdMaterialNetwork2 ConvertMaterialNetwork(
+    const HdMaterialNetworkSchema& network_schema) {
+  HdMaterialNetwork2 network;
+
+  HdMaterialNodeContainerSchema nodes_schema = network_schema.GetNodes();
+  if (!nodes_schema.IsDefined()) {
+    return network;
+  }
+
+  for (const TfToken& node_name : nodes_schema.GetNames()) {
+    HdMaterialNodeSchema node_schema = nodes_schema.Get(node_name);
+    if (!node_schema.IsDefined()) continue;
+    HdMaterialNode2 node;
+    node.nodeTypeId = GetParam<TfToken>(
+        node_schema.GetContainer(), HdMaterialNodeSchemaTokens->nodeIdentifier);
+
+    // Parameters
+    if (auto params_schema = node_schema.GetParameters()) {
+      for (const TfToken& param_name : params_schema.GetNames()) {
+        if (auto param_schema = params_schema.Get(param_name)) {
+          if (auto val_ds = param_schema.GetValue()) {
+            node.parameters[param_name] = val_ds->GetValue(0.0f);
+          }
+        }
+      }
+    }
+
+    // Input Connections
+    if (auto connections_schema = node_schema.GetInputConnections()) {
+      for (const TfToken& input_name : connections_schema.GetNames()) {
+        if (auto vector_schema = connections_schema.Get(input_name)) {
+          std::vector<HdMaterialConnection2> connections;
+          for (size_t i = 0; i < vector_schema.GetNumElements(); ++i) {
+            HdMaterialConnectionSchema conn_schema =
+                vector_schema.GetElement(i);
+            if (conn_schema.IsDefined()) {
+              HdMaterialConnection2 conn;
+              conn.upstreamNode = SdfPath(GetParam<TfToken>(
+                  conn_schema.GetContainer(),
+                  HdMaterialConnectionSchemaTokens->upstreamNodePath));
+              conn.upstreamOutputName = GetParam<TfToken>(
+                  conn_schema.GetContainer(),
+                  HdMaterialConnectionSchemaTokens->upstreamNodeOutputName);
+              connections.push_back(conn);
+            }
+          }
+          node.inputConnections[input_name] = connections;
+        }
+      }
+    }
+    network.nodes[SdfPath(node_name)] = node;
+  }
+
+  auto terminals_schema = network_schema.GetTerminals();
+  if (!terminals_schema) {
+    return network;
+  }
+
+  for (const TfToken& terminal_name : terminals_schema.GetNames()) {
+    if (auto conn_schema = terminals_schema.Get(terminal_name)) {
+      HdMaterialConnection2 conn;
+      conn.upstreamNode = SdfPath(GetParam<TfToken>(
+          conn_schema.GetContainer(),
+          HdMaterialConnectionSchemaTokens->upstreamNodePath));
+      conn.upstreamOutputName = GetParam<TfToken>(
+          conn_schema.GetContainer(),
+          HdMaterialConnectionSchemaTokens->upstreamNodeOutputName);
+      network.terminals[terminal_name] = conn;
+    }
+  }
+
+  return network;
+}
+
+}  // namespace
+
 HdMitsubaMaterial::HdMitsubaMaterial(const SdfPath& id) : HdMaterial(id) {}
 
 void HdMitsubaMaterial::Sync(HdSceneDelegate* scene_delegate,
@@ -39,20 +125,35 @@ void HdMitsubaMaterial::Sync(HdSceneDelegate* scene_delegate,
                              HdDirtyBits* dirty_bits) {
   const SdfPath& id = GetId();
   TF_DEBUG(HDMITSUBA_SYNC).Msg("HdMitsubaMaterial::Sync: %s\n", id.GetText());
-  if ((*dirty_bits & DirtyBits::DirtyParams) ||
-      (*dirty_bits & DirtyBits::DirtyResource)) {
-    auto* mitsuba_render_param =
-        static_cast<HdMitsubaRenderParam*>(render_param);
-    VtValue material_resource = scene_delegate->GetMaterialResource(id);
-    if (material_resource.IsHolding<HdMaterialNetworkMap>()) {
-      MaterialSpec spec;
-      spec.id = id;
-      spec.network2 = HdConvertToHdMaterialNetwork2(
-          material_resource.Get<HdMaterialNetworkMap>(), nullptr);
-      spec.needs_rebuild = true;
-      mitsuba_render_param->GetScene()->SyncMaterial(std::move(spec));
-    }
+
+  if (!(*dirty_bits & DirtyBits::DirtyParams) &&
+      !(*dirty_bits & DirtyBits::DirtyResource)) {
+    *dirty_bits = HdChangeTracker::Clean;
+    return;
   }
+
+  HdSceneIndexBaseRefPtr scene_index =
+      scene_delegate->GetRenderIndex().GetTerminalSceneIndex();
+  if (!TF_VERIFY(scene_index)) {
+    return;
+  }
+  HdMaterialSchema materialSchema =
+      HdMaterialSchema::GetFromParent(scene_index->GetPrim(id).dataSource);
+  if (!materialSchema.IsDefined()) {
+    return;
+  }
+  HdMaterialNetworkSchema networkSchema = materialSchema.GetMaterialNetwork();
+  if (!networkSchema.IsDefined()) {
+    return;
+  }
+
+  SceneManager* scene_manager =
+      static_cast<HdMitsubaRenderParam*>(render_param)->GetScene();
+  MaterialSpec spec;
+  spec.id = id;
+  spec.network2 = ConvertMaterialNetwork(networkSchema);
+  spec.needs_rebuild = true;
+  scene_manager->SyncMaterial(std::move(spec));
   *dirty_bits = HdChangeTracker::Clean;
 }
 
